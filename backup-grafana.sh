a7731ca03b73a052bc8c33b7565e7797dfa98815
pgarus <pgarus@users.noreply.github.com>
2017-08-04 11:43:52 +0200

    backup-grafana.sh added

---
+++ backup-grafana.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+DOCKER_COMPOSE_FILE="$1"
+SERVICE_NAME="$2"
+BACKUP_LOCATION="$3"
+
+SERVICE_CMD="docker-compose -f $DOCKER_COMPOSE_FILE exec -T $SERVICE_NAME"
+DATE=`date +%Y-%m-%d`
+
+# backup
+$SERVICE_CMD sh -c "cd $BACKUP_LOCATION; cp /var/lib/grafana/grafana.db grafana.db.$DATE  &&  gzip -9 grafana.db.$DATE"
+
+# keep only 3 newest backups
+$SERVICE_CMD sh -c "cd $BACKUP_LOCATION; find . -regex '\./grafana.db.\d\d\d\d-\d\d-\d\d\.gz' | sort -r | tail -n +4 | xargs -r rm"
+
